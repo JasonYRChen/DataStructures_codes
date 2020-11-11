e5c23f25d9b79d13243002653a2991396e939220
JasonYRChen <jkmylife@gmail.com>
2020-11-11 11:19:15 +0800

    comment and uncomment

--- ch5/dynamic_array.py
+++ ch5/dynamic_array.py
@@ -38,12 +38,12 @@ class Array:
 
 
 a = Array(1, 2, 3, 4, 5)
-print(a[1:])
-# print(f"array id: {id(a._array)}, array_size: {a.array_size}, element numbers: {len(a)}, size of array: {getsizeof(a._array)}")
-# print(f"repr. array: {a}")
-# print(f"real array: {a._array}", end='\n\n')
-# for n in range(100, 105):
-#     a.append(n)
-#     print(f"array id: {id(a._array)}, array_size: {a.array_size}, element numbers: {len(a)}, size of array: {getsizeof(a._array)}")
-#     print(f"repr. array: {a}")
-#     print(f"real array: {a._array}", end='\n\n')
+# print(a[1:])
+print(f"array id: {id(a._array)}, array_size: {a.array_size}, element numbers: {len(a)}, size of array: {getsizeof(a._array)}")
+print(f"repr. array: {a}")
+print(f"real array: {a._array}", end='\n\n')
+for n in range(100, 105):
+    a.append(n)
+    print(f"array id: {id(a._array)}, array_size: {a.array_size}, element numbers: {len(a)}, size of array: {getsizeof(a._array)}")
+    print(f"repr. array: {a}")
+    print(f"real array: {a._array}", end='\n\n')
