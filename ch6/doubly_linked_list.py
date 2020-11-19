533f822369c77f82868133196629701741098cf8
JasonYRChen <jkmylife@gmail.com>
2020-11-19 11:13:08 +0800

    (doubly_linked_list.py): New file added.

---
+++ ch6/doubly_linked_list.py
@@ -0,0 +1,9 @@
+class DoublyLinkedNode:
+    def __init__(self, val=0, prev_node=None, next_node=None):
+        self._val = val
+        self._prev = prev_node
+        self._next = next_node
+
+
+class Deque:
+    pass
