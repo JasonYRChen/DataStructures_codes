3756be0a8d281521f37e50e49b8cdb3e2400773a
JasonYRChen <jkmylife@gmail.com>
2020-11-24 14:23:45 +0800

    (doubly_linked_list_with_dummy_ends.py): complete all methods.

--- ch6/doubly_linked_list_with_dummy_ends.py
+++ ch6/doubly_linked_list_with_dummy_ends.py
@@ -10,6 +10,7 @@ class DoublyLinkedNode:
         self._next = next_node
 
     def __repr__(self):
+        # return f"{self._val}"
         return f"{self.__class__.__name__}(val={self._val}, prev={self._prev._val}, next={self._next._val})"
 
 
@@ -58,49 +59,118 @@ class Deque:
         return node._val
 
     def append(self, item):
-        pass
+        self._tail._prev = DoublyLinkedNode(item, self._tail._prev, self._tail)
+        self._tail._prev._prev._next = self._tail._prev
+        self._size += 1
 
     def appendleft(self, item):
-        pass
+        self._head._next = DoublyLinkedNode(item, self._head, self._head._next)
+        self._head._next._next._prev = self._head._next
+        self._size += 1
 
     def pop(self):
-        pass
+        if self._tail._prev is self._head:
+            raise ValueError('Empty list.')
+
+        pop_node = self._tail._prev
+        self._tail._prev, pop_node._prev._next = pop_node._prev, self._tail
+        pop_node._prev, pop_node._next = None, None
+        self._size -= 1
+        return pop_node._val
 
     def popleft(self):
-        pass
+        if self._head._next is self._tail:
+            raise ValueError('Empty list.')
+
+        pop_node = self._head._next
+        self._head._next, pop_node._next._prev = pop_node._next, self._head
+        pop_node._next, pop_node._prev = None, None
+        self._size -= 1
+        return pop_node._val
 
     def insert(self, index, item):
-        pass
+        index = self._valid_index(index)
+        node = self._head._next
+        for _ in range(index):
+            node = node._next
+        new_node = DoublyLinkedNode(item, node._prev, node)
+        node._prev._next, node._prev = new_node, new_node
+        self._size += 1
 
     def remove(self, index):
-        pass
+        index = self._valid_index(index)
+        node = self._head._next
+        for _ in range(index):
+            node = node._next
+        if node is self._tail:
+            raise IndexError('Empty list.')
+        node._prev._next, node._next._prev = node._next, node._prev
+        node._prev, node._next = None, None
+        self._size -= 1
 
     def rotate(self, shift):
-        pass
+        shift = len(self) - (shift % len(self))
+        self._head._next._prev, self._tail._prev._next = self._tail._prev, self._head._next
+        node = self._head._next
+        for _ in range(shift):
+            node = node._next
+        first, last = node, node._prev
+        first._prev, last._next = self._head, self._tail
+        self._head._next, self._tail._prev = first, last
 
     def extend(self, iters):
-        pass
+        if not iters:
+            raise ValueError('Nothing to extend')
+        first, last = self._build_nodes(iters)
+        node = self._tail._prev
+        first._prev, last._next = node, self._tail
+        node._next, self._tail._prev = first, last
 
     def extendleft(self, iters):
-        pass
+        if not iters:
+            raise ValueError('Nothing to extend')
+        first, last = self._build_nodes(iters)
+        node = self._head._next
+        first._prev, last._next = self._head, node
+        node._prev, self._head._next = last, first
 
     def clear(self):
-        pass
+        self._head._next._prev, self._tail._prev._next = None, None
+        self._head._next, self._head._prev = self._tail, self._tail
+        self._tail._next, self._tail._prev = self._head, self._head
+        self._size = 0
 
     def count(self, item):
-        pass
+        node = self._head._next
+        num = 0
+        for _ in range(len(self)):
+            if node._val == item:
+               num += 1
+            node = node._next
+        return num
 
     def reverse(self):
-        pass
+        node = self._head._next
+        for _ in range(len(self)):
+            node._next, node._prev = node._prev, node._next
+            node = node._prev
+        self._head._next._next, self._tail._prev._prev = self._tail, self._head
+        self._head._next, self._tail._prev = self._tail._prev, self._head._next
 
     def copy(self):
-        pass
+        iters = []
+        node = self._head._next
+        for _ in range(len(self)):
+            iters.append(node._val)
+            node = node._next
+        return Deque(iters)
 
     def _build_nodes(self, iters):
         if not isinstance(iters, Sequence):
             raise ValueError('Invalid sequence.')
 
         first = None
+        prev = None
         for item in iters:
             if first is None:
                 node = DoublyLinkedNode(item)
@@ -128,8 +198,12 @@ class Deque:
             stop = len(self) if step > 0 else -1
         elif stop < 0:
             stop += len(self)
-        is_interval = abs(stop - (start + step // abs(step))) < abs(stop - start)  # if start +/- 1 step is moving toward stop, then this is an effective interval
-        is_overlap = not((start > u_bound and stop >= u_bound) or (start < l_bound and stop <= l_bound))  # check if overlap to (0, len(self))
+
+        # Only if it's an interval, starts from 'start' and moves toward 'stop' through 'step',
+        # and the interval overlaps with list, which is the target, interval specified by l_bound
+        # and u_bound.
+        is_interval = abs(stop - (start + step // abs(step))) < abs(stop - start)
+        is_overlap = not((start > u_bound and stop >= u_bound) or (start < l_bound and stop <= l_bound))
         if is_interval and is_overlap:
             if start < l_bound:
                 start = l_bound
@@ -147,13 +221,12 @@ class Deque:
         if isinstance(index, slice):
             start, stop, step = self._slice_decompose(index)
             return (num for num in range(start, stop, step))
-
         if not isinstance(index, int):
             raise ValueError('Invalid index. Index should be an integer or a slice.')
 
         if index < 0:
             index += len(self)
-        if (len(self) > 0 and not 0 <= index < len(self)) or (len(self) == 0 and index != 0):
+        if (len(self) > 0 and not 0 <= index < len(self)) or (len(self) == 0 and (index != 0 and index != -1)):
             raise IndexError('Invalid index.')
         return index
 
@@ -165,6 +238,16 @@ class Deque:
 
 
 if __name__ == '__main__':
-    d = Deque(range(6))
+    d = Deque(range(5))
+    # d = Deque()
+    # d.rotate(-1)
+    # d.extend([])
+    # d.clear()
+    # d.extend([11, 22, 11, 22 ,11])
+    e = d.copy()
     d._list_all()
-    print(d[9:-10:-2])
+    print(d[:])
+    print()
+    e._list_all()
+    print(e[:])
+    print(id(d), id(e))
