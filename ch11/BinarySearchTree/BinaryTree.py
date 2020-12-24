34a98703c1b1d4555d460f28579015a759a8624b
JasonYRChen <jkmylife@gmail.com>
2020-12-24 23:14:30 +0800

    (BinaryTree.py): base class to be inherited

---
+++ ch11/BinarySearchTree/BinaryTree.py
@@ -0,0 +1,24 @@
+class BinaryTree:
+    def _parent(self, item):
+        raise NotImplementedError
+
+    def _left(self, item):
+        raise NotImplementedError
+
+    def _right(self, item):
+        raise NotImplementedError
+
+    def _root(self):
+        raise NotImplementedError
+
+    def _is_root(self, item):
+        raise NotImplementedError
+
+    def _is_leaf(self, item):
+        raise NotImplementedError
+
+    def _height(self, item):
+        raise NotImplementedError
+
+    def _depth(self, item):
+        raise NotImplementedError
