b6432b10b3e7061cd7860a59eeb36e2a908828b8
JasonYRChen <jkmylife@gmail.com>
2021-01-31 17:04:47 +0800

    (bucket_sort.py): finished

--- ch12/bucket_sort.py
+++ ch12/bucket_sort.py
@@ -1,2 +1,15 @@
+from random import randrange
+
+
 def bucket_sort(data, end):
-    
+    bucket = [[] for _ in range(end)]
+    for i in data:
+        bucket[i].append(i)
+    result = []
+    result.extend(num for item in bucket for num in item)
+    return result
+
+
+data = [randrange(5) for _ in range(20)]
+print(data)
+print(bucket_sort(data, 5))
