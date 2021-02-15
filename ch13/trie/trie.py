3625c8c278c8fef31933df57e274e6ef9754d307
JasonYRChen <jkmylife@gmail.com>
2021-02-15 23:08:55 +0800

    (trie.py): now can parse all words and symbols and reconstruct original message.

--- ch13/trie/trie.py
+++ ch13/trie/trie.py
@@ -1,4 +1,5 @@
 from ch13.trie.secondary_search_table import Dictionary
+from ch13.for_HuffmanCode.heap_remade import Heap
 
 
 class Trie:
@@ -12,14 +13,15 @@ class Trie:
         def __repr__(self):
             return f"{self.children}, {self.index}"
 
-    def __init__(self, paragraph=None, children_type=Dictionary):
+    def __init__(self, paragraph=None, children_type=Dictionary, parse_all=False):
         self.children_type = children_type
         self.root = self._Node(self.children_type())
+        self.parse_all = parse_all
         if paragraph:
             self._build(paragraph)
 
     def __repr__(self):
-        return f"Trie({', '.join(word for word in self._word_in_trie(self.root))})"
+        return f"Trie({', '.join(word for word, _ in self._word_in_trie(self.root))})"
 
     def _search(self, word):
         node = self.root
@@ -41,29 +43,42 @@ class Trie:
 
     @staticmethod
     def _parse(paragraph):
-        word = ''
-        index = None
-        for i, char in enumerate(paragraph):
-            if char.isalpha():
-                if word == '':
-                    index = i
-                word += char
-            else:
-                if word:
-                    yield index, word
-                    word = ''
+        start = end = 0
+        while end < len(paragraph):
+            if not paragraph[end].isalpha():
+                if start < end:
+                    yield start, paragraph[start:end]
+                    start = end
+                yield start, paragraph[start:end+1]
+                start += 1
+            end += 1
+        if start < end:
+            yield start, paragraph[start:end]
+
+    # def _parse(self, paragraph):
+    #     word = ''
+    #     index = None
+    #     for i, char in enumerate(paragraph):
+    #         if char.isalpha():
+    #             if word == '':
+    #                 index = i
+    #             word += char
+    #         else:
+    #             if word:
+    #                 yield index, word
+    #                 word = ''
 
     def _build(self, paragraph):
         for i, word in self._parse(paragraph):
             self._add_word(i, word)
 
     def _word_in_trie(self, node, word=''):
+        if node.index:
+            yield word, node.index
         if node.children:
             for c, node in node.children.items():
                 new_word = word + c
                 yield from self._word_in_trie(node, new_word)
-        else:
-            yield word
 
     def _yield_all(self, node, level=0):
         for char, node in node.children.items():
@@ -71,18 +86,28 @@ class Trie:
             yield from self._yield_all(node, level+1)
 
     def show_trie(self):
-        old_level = 0
+        end = False
         for level, char, node in self._yield_all(self.root):
-            if level <= old_level:
-                print(' ' * level, sep='', end='')
+            if end:
+                print(' ' * level, end='', sep='')
+                end = False
             print(char, sep='', end='')
-            if not node.children:
+            if node.index:
                 print('', node.index)
-            old_level = level
+                end = True
+
+    def reconstruction(self):
+        heap = Heap()
+        for word, indices in self._word_in_trie(self.root):
+            for index in indices:
+                heap[index] = word
+        return ''.join(heap.pop_min() for _ in range(len(heap)))
 
 
 if __name__ == '__main__':
-    p = "see a bear? sell stock! see a bull? buy stock! bid stock! bid stock! hear the bell? stop!"
+    # p = "see a bear? sell stock! see a bull? buy stock! bid stock! bid stock! hear the bell? stop!"
+    p = 'the thesis is all about thieves.'
     t = Trie(p)
     print(t)
     t.show_trie()
+    print(t.reconstruction())
