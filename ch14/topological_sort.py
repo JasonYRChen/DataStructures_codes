cb7760107cc26b353a955f1fad7c8334f63a91cc
JasonYRChen <jkmylife@gmail.com>
2021-03-02 14:51:49 +0800

    (topological_sort.py): add textboook's method. It's faster.

--- ch14/topological_sort.py
+++ ch14/topological_sort.py
@@ -1,6 +1,10 @@
 from ch14.graphADT_AdjMap import BaseGraphAdjMap
 
 
+class DirectedCycleOccur(Exception):
+    pass
+
+
 def topological_sort_core(graph, found):
     """
     :param graph:
@@ -79,6 +83,30 @@ def topological_sort_count(graph, found, count):
     return count[0]
 
 
+def topological_sort_textbook(graph):
+    """
+    This method only produce one of possibilities of topological sort, but it is
+    much faster than mine.
+    """
+    topo_sort = []
+    no_incoming = []
+    income_count = {}
+    for vertex in graph.vertices():
+        income_count[vertex] = graph.degree(vertex, False)
+        if income_count[vertex] == 0:
+            no_incoming.append(vertex)
+    while no_incoming:
+        vertex = no_incoming.pop()
+        topo_sort.append(vertex)
+        for v_oppo, edge in graph.incident_edges(vertex):
+            income_count[v_oppo] -= 1
+            if income_count[v_oppo] == 0:
+                no_incoming.append(v_oppo)
+    if len(topo_sort) != graph.vertex_count():
+        raise DirectedCycleOccur('This graph is not acyclic. Topological sorting terminates.')
+    return topo_sort
+
+
 if __name__ == '__main__':
     from string import ascii_letters as al
 
@@ -99,7 +127,7 @@ if __name__ == '__main__':
     g.insert_edge(vs[1], vs[0], True, 'b-a')
     g.insert_edge(vs[1], vs[2], True, 'b-c')
     g.insert_edge(vs[3], vs[4], True, 'd-e')
-    g.insert_edge(vs[2], vs[4], True, 'c-e')
+    g.insert_edge(vs[6], vs[4], True, 'g-e')
     g.insert_edge(vs[4], vs[7], True, 'e-h')
     g.insert_edge(vs[5], vs[6], True, 'f-g')
     g.insert_edge(vs[5], vs[2], True, 'f-c')
@@ -113,3 +141,5 @@ if __name__ == '__main__':
     for comb in topological_sort_all(g):
         print(comb)
     print(topological_sort_count(g, {}, [0]))
+    print()
+    print(topological_sort_textbook(g))
