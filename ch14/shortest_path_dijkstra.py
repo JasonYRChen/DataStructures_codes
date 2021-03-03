9290d017e024d04328dd40381db46da2effc4b9f
JasonYRChen <jkmylife@gmail.com>
2021-03-03 15:44:43 +0800

    (shortest_path_dijkstra.py): add simplified version and renew testing conditions.

--- ch14/shortest_path_dijkstra.py
+++ ch14/shortest_path_dijkstra.py
@@ -4,6 +4,10 @@ from math import inf
 
 
 def dijkstra_method(graph, start):
+    """ This function can detect if start vertex can reach to any vertices in the
+        rest of the graph. If any vertex cannot be reached, then
+        distances[vertex cannot be reached] = inf.
+    """
     heap = Heap([(0, start)])
     found = set()
     distances = {v: inf for v in graph.vertices()}
@@ -19,6 +23,23 @@ def dijkstra_method(graph, start):
     return distances
 
 
+def dijkstra_method_simplify(graph, start):
+    """ This function cannot report unreachable vertices at the beginning. One
+        should compare the distances's key with graph's vertices to show those
+        unreachable vertices.
+    """
+    heap = Heap([(0, start)])
+    distances = {}
+    while heap and len(distances) < graph.vertex_count():
+        distance, vertex = heap.pop_min()
+        if vertex not in distances:
+            distances[vertex] = distance
+            for v_oppo, edge in graph.incident_edges(vertex):
+                if v_oppo not in distances:
+                    heap[edge.element+distances[vertex]] = v_oppo
+    return distances
+
+
 if __name__ == '__main__':
     g = BaseGraphAdjMap()
     g.insert_vertex('BOS')
@@ -28,6 +49,8 @@ if __name__ == '__main__':
     g.insert_vertex('DFW')
     g.insert_vertex('LAX')
     g.insert_vertex('SFO')
+    g.insert_vertex('PVD')
+    g.insert_vertex('BWI')
     vs = {v.element: v for v in g.vertices()}
     g.insert_edge(vs['BOS'], vs['SFO'], False, 2704)
     g.insert_edge(vs['BOS'], vs['ORD'], False, 867)
@@ -41,6 +64,15 @@ if __name__ == '__main__':
     g.insert_edge(vs['MIA'], vs['LAX'], False, 2342)
     g.insert_edge(vs['DFW'], vs['SFO'], False, 1464)
     g.insert_edge(vs['DFW'], vs['LAX'], False, 1235)
-    print(g)
-    distances = dijkstra_method(g, vs['JFK'])
-    print(distances)
+    g.insert_edge(vs['SFO'], vs['LAX'], False, 337)
+    g.insert_edge(vs['PVD'], vs['ORD'], False, 849)
+    g.insert_edge(vs['PVD'], vs['JFK'], False, 144)
+    g.insert_edge(vs['BWI'], vs['JFK'], False, 184)
+    g.insert_edge(vs['BWI'], vs['MIA'], False, 946)
+    g.insert_edge(vs['BWI'], vs['ORD'], False, 621)
+    # print(g)
+    departure = 'BWI'
+    d1 = dijkstra_method(g, vs[departure])
+    d2 = dijkstra_method_simplify(g, vs[departure])
+    print(d1)
+    print(d1 == d2)
